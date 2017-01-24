c4521d4c86ae0e4990649fdf83b9fe316270d870
abc123mewot <abc123mewot@localhost>
2017-01-24 10:23:24 -0500

    Added FOV and Look direction to draw method

--- src/net/net16/jeremiahlowe/fighters/fighter/Fighter.java
+++ src/net/net16/jeremiahlowe/fighters/fighter/Fighter.java
@@ -9,8 +9,8 @@ import net.net16.jeremiahlowe.fighters.Rotation;
 
 public class Fighter {
 	public Vector2 position = new Vector2(0, 0);
-	public Rotation fov = new Rotation(105);
-	public Rotation looking = new Rotation(23);
+	public Rotation fov = new Rotation(30);
+	public Rotation looking = new Rotation(0);
 	public Team team = Team.Red;
 	public boolean visible = false;
 	public float fighterSize = 24;
@@ -31,30 +31,21 @@ public class Fighter {
 	public void draw(Graphics g, int w, int h){
 		if(visible){
 			Color teamColor = team.getColor();
-			drawBody(g, teamColor);
-			drawLooking(g, teamColor, w, h);
+			//Draw body
+			GraphicsUtil.fillCenteredCircle(fighterSize, position, g, teamColor);
+			GraphicsUtil.drawCenteredCircle(fighterSize, position, g, Color.black);
+			GraphicsUtil.drawCenteredCircle(fighterSize - 2, position, g, Color.black);
+			//Draw look direction
+			GraphicsUtil.drawAngle(g, teamColor, looking, position, w + h);
+			//Draw FOV group
+			Rotation fov1 = null, fov2 = null;
+			fov1 = new Rotation(fov.getAngleDegrees() / 2 + looking.getAngleDegrees());
+			fov2 = new Rotation(looking.getAngleDegrees() - fov.getAngleDegrees() / 2);
+			GraphicsUtil.drawAngle(g, teamColor, fov1, position, w + h);
+			GraphicsUtil.drawAngle(g, teamColor, fov2, position, w + h);
 		}
 	}
-	private void drawFOV(Graphics g, Color temColor, int w, int h){
-		
-	}
-	private void drawLooking(Graphics g, Color teamColor, int w, int h){
-		g.setColor(teamColor);
-		float xf = position.x, yf = position.y;
-		
-		float len = 400;
-		
-		float x2 = (float) (xf + len * Math.cos(looking.getAngleRadians()));
-		float y2 = (float) (yf + len * Math.sin(looking.getAngleRadians()) * -1);
-		
-		int x1 = Math.round(xf), y1 = Math.round(yf);
-		g.drawLine(0, y1, w, y1);
-		g.drawLine(x1, 0, x1, h);
-		g.drawLine(x1, y1, Math.round(x2), Math.round(y2));
-	}
-	private void drawBody(Graphics g, Color teamColor){
-		GraphicsUtil.fillCenteredCircle(fighterSize, position, g, teamColor);
-		GraphicsUtil.drawCenteredCircle(fighterSize, position, g, Color.black);
-		GraphicsUtil.drawCenteredCircle(fighterSize - 2, position, g, Color.black);
+	public void setRotation(Rotation looking){
+		this.looking = looking;
 	}
 }
